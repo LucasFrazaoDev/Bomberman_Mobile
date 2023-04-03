0af0f2a2d7356f3659465b07cbe447cd6642ba77
Lucas Frazão <lucas_devfrazao@outlook.com>
2023-04-03 10:28:22 -0300

    Main Menu UI changes, Localization imported

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 #if UNITY_EDITOR
 using UnityEditor;
@@ -8,10 +9,17 @@ using UnityEditor;
 
 public class MainMenuManager : MonoBehaviour
 {
+    [SerializeField] private GameObject _panelOptions;
+
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(1);
     }
 
     public void QuitGame()
@@ -20,5 +28,15 @@ public class MainMenuManager : MonoBehaviour
         EditorApplication.ExitPlaymode();
 #endif
         Application.Quit();
+    }
+
+    public void OpenPanelOptions()
+    {
+        _panelOptions.SetActive(true);
+    }
+    
+    public void ClosePanelOptions()
+    {
+        _panelOptions.SetActive(false);
     }
 }
